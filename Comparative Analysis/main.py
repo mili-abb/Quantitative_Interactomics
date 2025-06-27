7bd499338c665893fc4dab19bcbb5c071b63ecbe
mili-abb <abboutemilissapro@gmail.com>
2025-06-27 16:30:17 +0200

    main script done and actualisation analysis notebook

--- Comparative Analysis/main.py
+++ Comparative Analysis/main.py
@@ -1,31 +1,34 @@
-import sys
-sys.path.append('./')  # Add current directory to the Python path
-sys.path.append('./lib')  # Add 'lib' subdirectory
+'''This script serves as the main entry point for the Comparative Analysis project.
+It imports necessary modules and functions from the project, allowing for the execution of various analyses related to
+complexome data, including species bait extraction, CSV file operations, calculations of Kapp and complexome,
+comparison of complexomes, plotting Venn diagrams, and performing linear regression analysis.'''
 
-from calcul import (
-    calculate_Kapp,
-    get_bait_concentration,
-    calculate_complexome,
-    log_scale
-)
+__version__ = "0.1.1"  # Version of the script
+__author__ = "Milissa Abboute"
+__email__ = "abboutemilissapro@gmail.com"
+__date__ = "2025-06-27"  # Date of the last update
 
-from file_op import (
+##-- IMPORT LIBRARIES --##
+import os
+from lib.file_op import (
     extract_species_bait,
     read_csv_file,
     save_csv_file,
     merge_files,
     group_columns_by_species
 )
-
-from comparison import (
+from lib.calcul import (
+    calculate_Kapp,
+    get_bait_concentration,
+    calculate_complexome,
+    log_scale
+)
+from lib.comparison import (
     calculate_deltapKapp,
     calculate_complexome_difference,
     log_delta,
     calculate_intraspecies_log_complexome_ratio
 )
-
-from Venn_diag import (plot_venn_diagram)
-
-from complexome_plot import (plot_complexome_analysis)
-
-from lin_reg import (analyze_and_plot_correlation)
+from lib.Venn_diag import plot_venn_diagram
+from lib.complexome_plot import plot_complexome_analysis
+from lib.lin_reg import analyze_and_plot_correlation
